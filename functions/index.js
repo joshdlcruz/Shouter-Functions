8cbc25f92409d2099fd03981d7e185745de6bdb0
joshdlcruz <dcjoshua25@gmail.com>
2020-06-23 15:04:05 +0800

    scream validation

--- functions/index.js
+++ functions/index.js
@@ -44,7 +44,12 @@ app.get('/screams', (req, res) => {
     .catch((err) => console.error(err));
 });
 
-app.post('/screams', (req, res) => {
+// Post scream
+app.post('/scream', (req, res) => {
+  if (req.body.body.trim() === '') {
+    return res.status(400).json({ body: 'Body must not be empty' });
+  }
+
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
