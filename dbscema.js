64e7d6378a89f4ea13b55353df12ff14583152cc
joshdlcruz <dcjoshua25@gmail.com>
2020-06-23 10:46:25 +0800

    express & formatting response

---
+++ dbscema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'scream body',
+            createdAt: '2020-06-23T02:37:38.881Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
