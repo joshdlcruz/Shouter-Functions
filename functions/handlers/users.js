0a6731d758327e44c058a3b40c7842849a6a2419
joshdlcruz <dcjoshua25@gmail.com>
2020-06-24 07:21:37 +0800

    refactor

---
+++ functions/handlers/users.js
@@ -0,0 +1,89 @@
+const { fs } = require('../util/admin');
+const config = require('../util/config');
+const firebase = require('firebase');
+const { validateSignup, validateLogin } = require('../util/validators');
+
+firebase.initializeApp(config);
+
+exports.signUp = (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  const { valid, errors } = validateSignup(newUser);
+
+  if (!valid) return res.status(400).json(errors);
+
+  let token, userId;
+  fs.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: 'this handle is already taken' });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+
+      return fs.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === 'auth/email-already-in-use') {
+        return res.status(400).json({ email: 'email is already taken' });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+};
+
+exports.logIn = (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  const { valid, errors } = validateLogin(user);
+
+  if (!valid) return res.status(400).json(errors);
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then((data) => {
+      return data.user.getIdToken();
+    })
+    .then((token) => {
+      return res.json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === 'auth/wrong-password') {
+        return res
+          .status(403)
+          .json({ general: 'Wrong credentials, please try again' });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+};
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: 'AIzaSyChjblnOPrqpFvTV6QVJE1r0BVPHnL2bDs',
+    authDomain: 'socialape-e2298.firebaseapp.com',
+    databaseURL: 'https://socialape-e2298.firebaseio.com',
+    projectId: 'socialape-e2298',
+    storageBucket: 'socialape-e2298.appspot.com',
+    messagingSenderId: '343547952008',
+    appId: '1:343547952008:web:03435e7b6c0ff55d4bd83d',
+    measurementId: 'G-YC7ER77J9G',
+  };
