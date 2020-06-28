70d61d09fd279c9734239ed9f66538feda89851b
joshdlcruz <dcjoshua25@gmail.com>
2020-06-28 17:04:16 +0800

    finishing up

--- functions/index.js
+++ functions/index.js
@@ -106,3 +106,60 @@ exports.createNotificationOnComment = functions
         return;
       });
   });
+
+exports.onUserImageChange = functions
+  .region('asia-northeast1')
+  .firestore.document('/users/{userId}')
+  .onUpdate((change) => {
+    console.log(change.before.data());
+    console.log(change.after.data());
+    if (change.before.data().imageUrl !== change.after.data().imageUrl) {
+      console.log('image has changed');
+      const batch = fs.batch();
+      return fs
+        .collection('screams')
+        .where('userHandle', '==', change.before.data().handle)
+        .get()
+        .then((data) => {
+          data.forEach((doc) => {
+            const scream = fs.doc(`/screams/${doc.id}`);
+            batch.update(scream, { userImage: change.after.data().imageUrl });
+          });
+          return batch.commit();
+        });
+    } else return true;
+  });
+
+exports.onScreamDelete = functions
+  .region('asia-northeast1')
+  .firestore.document('/screams/{screamId}')
+  .onDelete((snapshot, context) => {
+    const screamId = context.params.screamId;
+    const batch = fs.batch();
+    return fs
+      .collection('comments')
+      .where('screamId', '==', screamId)
+      .get()
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(fs.doc(`/comments/${doc.id}`));
+        });
+        return fs.collection('likes').where('screamId', '==', screamId).get();
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(fs.doc(`/likes/${doc.id}`));
+        });
+        return fs
+          .collection('notifications')
+          .where('screamId', '==', screamId)
+          .get();
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(fs.doc(`/notifications/${doc.id}`));
+        });
+        return batch.commit();
+      })
+      .catch((err) => console.error(err));
+  });
